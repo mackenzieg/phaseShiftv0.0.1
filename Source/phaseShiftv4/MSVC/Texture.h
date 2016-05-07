6eb1ee0f48cae97fa04d11b46b81da92f47dd2d6
Nicholas Laskus <time.continnum@gmail.com>
2016-05-07 12:40:14 -0400

    Texture Added

---
+++ Source/phaseShiftv4/MSVC/Texture.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "PSPlatform/Windows/OpenGLClass.h"
+
+class Texture
+{
+private:
+	struct TGAHEADER
+	{
+		unsigned char Data1[12];
+		unsigned char Width;
+		unsigned char Height;
+		unsigned char BPP;
+		unsigned char Data2;
+	};
+public:
+	Texture();
+	Texture(const Texture&);
+	~Texture();
+
+	bool Initialzize(OpenGLClass*, char*, unsigned int, bool);
+	void Shutdown();
+
+private:
+	bool LoadTGA(OpenGLClass*, char*, unsigned int, bool);
+
+	bool loaded;
+	GLuint m_textureID;
+};
